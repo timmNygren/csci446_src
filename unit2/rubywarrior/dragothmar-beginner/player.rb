2e66d147538435908e4ed0567b5f7f8eefbf89b2
Timm Nygren <tnygren@mines.edu>
2014-01-18 14:28:07 -0700

    Level 4 completed

--- unit2/rubywarrior/dragothmar-beginner/player.rb
+++ unit2/rubywarrior/dragothmar-beginner/player.rb
@@ -1,14 +1,52 @@
 class Player
-	@health = warrior.health
-	
+
   def play_turn(warrior)
     # add your code here
-    if warrior.health < 20 and !warrior.feel.enemy?
+
+    # @state(warrior)
+    if warrior.health < 20 and @health <= warrior.health
     	warrior.rest!
   	elsif warrior.feel.enemy?
   		warrior.attack!
   	else
   		warrior.walk!
   	end
+
+  	@health = warrior.health
   end
+
+  # def self.update(warrior)
+  # 	@state(warrior)
+  # end
+
+  # def rest(warrior)
+  # 	if @health < 20
+  # 		warrior.rest!
+  # 	else
+  # 		@state = move_forward
+  # 		self.update(warrior)
+  # 	end
+  # end
+
+  # def move_forward(warrior)
+  # 	if warrior.feel.enemy?
+  # 		@state = attack
+  # 		self.update(warrior)
+  # 	elsif @health - warrior.health == 0 and @health != 20
+  # 		@state = rest
+  # 		self.update(warrior)
+  # 	else
+  # 		warrior.walk!
+  # 	end
+  # end
+
+  # def attack(warrior)
+  # 	if !warrior.feel.enemy?
+  # 		@state = move_forward
+  # 		self.update(warrior)
+  # 	else
+  # 		warrior.attack!
+  # 	end
+  # end
+
 end
