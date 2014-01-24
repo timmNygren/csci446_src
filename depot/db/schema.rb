c0b05e79ba583387afebc854233010386a3820d6
Timm Nygren <tnygren@mines.edu>
2014-01-23 23:27:02 -0700

    Iteration D1 complete

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124003447) do
+ActiveRecord::Schema.define(version: 20140124060623) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
