fb6424853257b4c0323c9a5c0300013b42dc9289
Timm Nygren <tnygren@mines.edu>
2014-02-04 14:50:33 -0700

    Fixed not being able to delete foster pets from the page, added validation to form fields.

--- pets/app/models/pet.rb
+++ pets/app/models/pet.rb
@@ -1,2 +1,4 @@
 class Pet < ActiveRecord::Base
+	validates :name, :breed, :image_url, :coloring, :habits, :gender, :presence => true
+	validates :age, :numericality => {:greater_than_or_equal_to => 1}
 end
