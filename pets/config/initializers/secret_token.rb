41967dfb546f2a134cd5848c9db347e55a36975f
Timm Nygren <tnygren@mines.edu>
2014-02-03 19:12:56 -0700

    Pets app skeleton created

---
+++ pets/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pets::Application.config.secret_key_base = '2028253c6fbcd87221c135ac13fac57d347a524e585ad1ba48d414e10a5cd3f74be21930672c29ae2f7071d9e7bdf160433c628b5e526546df3ee2fedf228356'
