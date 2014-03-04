c85511319c14b96b16d21575eb568de43d260e95
Timm Nygren <tnygren@mines.edu>
2014-03-03 21:21:04 -0700

    List loads and sorts properly, added some awesome but simple css.

--- books/simple_rack.rb
+++ books/simple_rack.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/ruby
 
 require 'rack'
+require 'CSV'
 
 class SimpleApp
 	def initialize()
     # can set up variables that will be needed later
-    CSV.foreach("bookList.csv") do |row|
-      puts row
-    end
+    @bookList = []
+    load_books
 		@time = Time.now
-    puts "This is a test"
 	end
 
 	def call(env)
@@ -24,12 +23,12 @@ class SimpleApp
         # remove leading /
         file = env["PATH_INFO"][1..-1]
         return [200, {"Content-Type" => "text/css"}, [File.open(file, "rb").read]]
-      when /\/crazy.*/
+      when /\/form.*/
         # serve up the form
-        render_crazy(request, response)
-      when /\/goofy.*/
+        render_form(request, response)
+      when /\/list.*/
         # serve up a list response
-        render_goofy(request, response)
+        render_list(request, response)
       else
         [404, {"Content-Type" => "text/plain"}, ["Error 404!"]]
     end	# end case
@@ -37,20 +36,67 @@ class SimpleApp
       response.finish
   end
 
-  # try http://localhost:8080/crazy
-	def render_crazy(req, response)
+  # try http://localhost:8080/form
+	def render_form(req, response)
     File.open("_form.html", "r") { |head| response.write(head.read)}
 	end
 
-  # try http://localhost:8080/goofy?name=Jezebel
-	def render_goofy(req, response)
-		whoIsGoofy = req.GET["sortorder"]
-		response.write( "<h2>Proclamation</h2>\n" )
-    response.write("<p>#{whoIsGoofy} is goofy!")
+  # try http://localhost:8080/list?sortorder=Author
+	def render_list(req, response)
+		sortorder = req.GET["sortorder"]
+    case sortorder
+    when "Title"
+      sort = :title
+    when "Author"
+      sort = :author
+    when "Language"
+      sort = :language
+    when "Year"
+      sort = :year
+    else
+      puts "An error with sort selections."
+      sort = :title
+    end
+    response.write("<h2>Sorted by #{sort}</h2>")
+    @bookList.sort! { |book1, book2| book1[sort] <=> book2[sort]}
+    render_table(req, response, sort)
 	end
 
   def render_footer(req, response)
     response.write("<footer><br /><small>Brought to you by: We Grok Books</small></footer>")
+  end
+
+  def render_table(req, response, sort)
+    response.write("<table>")
+    response.write("<tr>")
+    response.write("<th>Rank</th>")
+    response.write("<th>Title</th>")
+    response.write("<th>Author</th>")
+    response.write("<th>Language</th>")
+    response.write("<th>Year</th>")
+    response.write("<th>Copies</th>")
+    response.write("</tr>")
+    @bookList.each do |book|
+      response.write("<tr>")
+      book.each do |attribute|
+        if (attribute[0] == sort)
+          response.write("<td id='sort'>#{attribute[1]}")
+        else
+          response.write("<td>#{attribute[1]}")
+        end
+      end
+      response.write("</tr>")
+    end
+    response.write("</table>")
+  end
+
+  def load_books
+    counter = 1
+    CSV.foreach("bookList.csv") do |row|
+      tempItem = {:rank => counter, :title => row[0], :author => row[1], :language => row[2], :year => row[3], :copies => row[4]}
+      @bookList << tempItem  
+      counter += 1
+    end
   end
 end
 
